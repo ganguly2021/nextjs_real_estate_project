ed5c21f180d36735605b7a5fa213cd33a17d0380
ganguly2021 <sandeepganguly21@gmail.com>
2022-02-13 16:50:56 +0530

    search filters select url query change set.

--- components/SearchFilters.js
+++ components/SearchFilters.js
@@ -10,15 +10,50 @@ import {
 } from "@chakra-ui/react";
 
 import { useState, useEffect } from "react";
-import { useRouter } from "next/router";
+import { Router, useRouter } from "next/router";
 import { MdCancel } from "react-icons/md";
 import Image from "next/image";
+import { filterData, getFilterValues } from "./../utils/filterData";
 
 function SearchFilters() {
-  const [filters, setFilters] = useState({});
+  const [filters, setFilters] = useState(filterData);
 
+  const router = useRouter();
 
-  return <div>SearchFilters</div>;
+  const searchProperties = (filterValues) => {
+    const path = router.pathname;
+
+    const { query } = router;
+
+    const values = getFilterValues(filterValues);
+
+    values.forEach(item => {
+      query[item.name] = item.value;
+    });
+  
+    router.push({ pathname: path, query })
+  };
+
+  return (
+    <Flex bg="gray.100" p="4" justifyContent="center" flexWrap="wrap">
+      {filters.map((filter) => (
+        <Box key={filter.queryName}>
+          <Select
+            placeholder={filter.placeholder}
+            w="fit-content"
+            p="2"
+            onChange={(e) =>
+              searchProperties({ [filter.queryName]: e.target.value })
+            }
+          >
+            {filter?.items?.map((item) => (
+              <option value={item.value}>{item.name}</option>
+            ))}
+          </Select>
+        </Box>
+      ))}
+    </Flex>
+  );
 }
 
 export default SearchFilters;
