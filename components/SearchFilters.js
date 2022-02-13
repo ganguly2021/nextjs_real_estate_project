1569c7d8467f8807da2706efba5859be52ab9ccd
ganguly2021 <sandeepganguly21@gmail.com>
2022-02-13 16:28:28 +0530

    search filter getServerSideProp setup.

--- components/SearchFilters.js
+++ components/SearchFilters.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import {
+  Flex,
+  Select,
+  Box,
+  Text,
+  Input,
+  Spinner,
+  Icon,
+  Button,
+} from "@chakra-ui/react";
+
+import { useState, useEffect } from "react";
+import { useRouter } from "next/router";
+import { MdCancel } from "react-icons/md";
+import Image from "next/image";
 
 function SearchFilters() {
-  return (
-    <div>SearchFilters</div>
-  )
+  return <div>SearchFilters</div>;
 }
 
-export default SearchFilters
+export default SearchFilters;
--- pages/search.js
+++ pages/search.js
@@ -5,8 +5,11 @@ import Image from "next/image";
 import { BsFilter } from "react-icons/bs";
 import { Flex, Box, Text, Icon } from "@chakra-ui/react";
 import SearchFilters from "../components/SearchFilters";
+import Property from "../components/Property";
+import noResult from "./../assets/images/noresult.svg";
+import { baseUrl, fetchApi } from './../utils/fetchApi';
 
-function SearchPage() {
+function SearchPage({ properties }) {
   const [searchFilters, setSearchFilters] = useState(false);
   const router = useRouter();
 
@@ -27,9 +30,54 @@ function SearchPage() {
         <Text>Seach Property By Filters</Text>
         <Icon as={BsFilter} paddingLeft="2" w="7" />
       </Flex>
-      { searchFilters && <SearchFilters /> }
+      {searchFilters && <SearchFilters />}
+      <Text fontSize="2xl" p="4" fontWeight="bold">
+        Properties {router.query.purpose}
+      </Text>
+      <Flex flexWrap="wrap">
+        {properties.map((property) => (
+          <Property property={property} key={property.id} />
+        ))}
+      </Flex>
+      {properties.length === 0 && (
+        <Flex
+          justifyContent="center"
+          alignItems="center"
+          flexDirection="column"
+          marginTop="5"
+          marginBottom="5"
+        >
+          <Image src={noResult} alt="no-result" />
+          <Text fontSize="2xl" marginTop="3">
+            No results found
+          </Text>
+        </Flex>
+      )}
     </Box>
   );
 }
 
 export default SearchPage;
+
+export async function getServerSideProps({ query }) {
+  const purpose = query.purpose || "for-rent";
+  const rentFrequency = query.rentFrequency || "yearly";
+  const minPrice = query.minPrice || "0";
+  const maxPrice = query.maxPrice || "1000000";
+  const roomsMin = query.roomsMin || "0";
+  const bathsMin = query.bathsMin || "0";
+  const sort = query.sort || "price-desc";
+  const areaMax = query.areaMax || "35000";
+  const locationExternalIDs = query.locationExternalIDs || "5002";
+  const categoryExternalID = query.categoryExternalID || "4";
+
+  const data = await fetchApi(
+    `${baseUrl}/properties/list?locationExternalIDs=${locationExternalIDs}&purpose=${purpose}&categoryExternalID=${categoryExternalID}&bathsMin=${bathsMin}&rentFrequency=${rentFrequency}&priceMin=${minPrice}&priceMax=${maxPrice}&roomsMin=${roomsMin}&sort=${sort}&areaMax=${areaMax}`
+  );
+
+  return {
+    props: {
+      properties: data?.hits,
+    },
+  };
+}
