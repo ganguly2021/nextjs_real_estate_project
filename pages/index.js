611769dde9c0210cb361b27affff97fa850550f0
ganguly2021 <sandeepganguly21@gmail.com>
2022-02-13 14:59:21 +0530

    Property component setup.

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import { Flex, Box } from "@chakra-ui/react";
 import styles from "./../styles/Home.module.css";
 
 import Banner from "./../components/Banner";
+import Property from '../components/Property'
 import { baseUrl, fetchApi } from "./../utils/fetchApi";
 
 export default function Home({ propertiesForRent, propertiesForSale }) {
@@ -19,7 +20,12 @@ export default function Home({ propertiesForRent, propertiesForSale }) {
         linkName="/search?purpose=for-rent"
         imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/145426814/33973352624c48628e41f2ec460faba4"
       />
-      <Flex flexWrap="wrap">{/* Fetch data from API */}</Flex>
+      <Flex flexWrap="wrap">
+        {/* Fetch data from API for property rent */}
+        {propertiesForRent.map((property) => (
+          <Property property={property} key={property.id} />
+        ))}
+      </Flex>
       <Banner
         purpose="BUY A HOME"
         title1="Find, Buy & Own Your"
@@ -30,7 +36,12 @@ export default function Home({ propertiesForRent, propertiesForSale }) {
         linkName="/search?purpose=for-sale"
         imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/110993385/6a070e8e1bae4f7d8c1429bc303d2008"
       />
-      <Flex flexWrap="wrap">{/* Fetch data from API */}</Flex>
+      <Flex flexWrap="wrap">
+        {/* Fetch data from API for property sale */}
+        {propertiesForSale.map((property) => (
+          <Property property={property} key={property.id} />
+        ))}
+      </Flex>
     </Box>
   );
 }
