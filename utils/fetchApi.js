011d8c204e3919cee68d2ff5f0a97f85c7f91688
ganguly2021 <sandeepganguly21@gmail.com>
2022-02-13 21:00:48 +0530

    some changes.

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -6,7 +6,7 @@ export const baseUrl = "https://bayut.p.rapidapi.com";
 // api headers
 const headers = {
   "x-rapidapi-host": "bayut.p.rapidapi.com",
-  "x-rapidapi-key": "abdbe95236msh9a4349c53ede780p116d40jsn7d9e0fdd3c01",
+  "x-rapidapi-key": "",
 };
 
 export const fetchApi = async (url) => {
